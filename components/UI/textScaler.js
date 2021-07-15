aad43dd2ad99d7b586adc747604a05a194a62e4d
unknown <inariwind@gmail.com>
2021-07-16 00:23:57 +0300

    Minor update.

--- components/UI/textScaler.js
+++ components/UI/textScaler.js
@@ -8,6 +8,9 @@ const textScaler = (textPosition, textScaleY) => {
     // Rules for between 50% and 45%.
     // Between 45% and 5% the text size should not change, so the user can actually read it in the middle of the screen.
     return (textScaleY = (10 - textPosition / 10) / 5);
+    // To scale the text, we need numbers below 10, hence the division by 10.
+    // the 10 at the start is because we scroll down and the numbers begin from above 100, so we actually need the difference.
+    // the last number division can be considered a further factor step.
   } else if (textPosition < 5 && textPosition > 0) {
     // Rules for between 5% and 0%.
     return (textScaleY = textPosition / 10 / 4);
