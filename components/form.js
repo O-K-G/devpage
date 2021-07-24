08de7b23afc36ba5207368e8275c0ee555004b3e
unknown <inariwind@gmail.com>
2021-07-24 17:13:02 +0300

    Add RTL support.

--- components/form.js
+++ components/form.js
@@ -2,7 +2,13 @@ import React, { useState } from "react";
 import classes from "./form.module.css";
 
 const Form = () => {
-  const [value, setValue] = useState({ fName: "", lName: "", email: "", message: "", rtl: false });
+  const [value, setValue] = useState({
+    fName: "",
+    lName: "",
+    email: "",
+    message: "",
+    rtl: false,
+  });
 
   const handleChange = (e) => {
     setValue((prevValue) => ({
@@ -10,25 +16,39 @@ const Form = () => {
       [e.target.name]: e.target.value,
     }));
   };
-    
-    const handleSubmit = async (e) => {
-      e.preventDefault();
-      try {
+
+  const handleRtl = () => {
+    setValue((prevValue) => ({
+      ...prevValue,
+      rtl: true,
+    }));
+  };
+
+  const handleLtr = () => {
+    setValue((prevValue) => ({
+      ...prevValue,
+      rtl: false,
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
       const response = await fetch("/api/send", {
-        method: 'POST',
-       mode: 'cors',
-       credentials: 'include',
+        method: "POST",
+        mode: "cors",
+        credentials: "include",
         headers: {
-          'Content-Type': 'application/json'
+          "Content-Type": "application/json",
         },
-        body: JSON.stringify(value)
+        body: JSON.stringify(value),
       });
       const reply = await response.json();
       console.log(reply.message);
     } catch (error) {
       console.warn(error);
     }
-    }
+  };
 
   return (
     <form onSubmit={handleSubmit} className={classes.form}>
@@ -39,6 +59,7 @@ const Form = () => {
         value={value.fName}
         onChange={handleChange}
         className={classes.input}
+        dir={value.rtl ? "rtl" : "ltr"}
       />
       <br />
       <br />
@@ -49,6 +70,7 @@ const Form = () => {
         value={value.lName}
         onChange={handleChange}
         className={classes.input}
+        dir={value.rtl ? "rtl" : "ltr"}
       />
       <br />
       <br />
@@ -71,10 +93,33 @@ const Form = () => {
         className={classes.input}
         rows="10"
         cols="30"
+        dir={value.rtl ? "rtl" : "ltr"}
       />
       <br />
       <br />
       <input className={classes.input} type="submit" value="Submit" />
+
+      <svg
+        focusable="false"
+        viewBox="0 0 24 24"
+        aria-hidden="true"
+        data-testid="FormatAlignLeftIcon"
+        style={{ fill: "red", width: "32px" }}
+        onClick={handleLtr}
+      >
+        <path d="M15 15H3v2h12v-2zm0-8H3v2h12V7zM3 13h18v-2H3v2zm0 8h18v-2H3v2zM3 3v2h18V3H3z"></path>
+      </svg>
+
+      <svg
+        focusable="false"
+        viewBox="0 0 24 24"
+        aria-hidden="true"
+        data-testid="FormatAlignRightIcon"
+        style={{ fill: "red", width: "32px" }}
+        onClick={handleRtl}
+      >
+        <path d="M3 21h18v-2H3v2zm6-4h12v-2H9v2zm-6-4h18v-2H3v2zm6-4h12V7H9v2zM3 3v2h18V3H3z"></path>
+      </svg>
     </form>
   );
 };
--- pages/api/send.js
+++ pages/api/send.js
@@ -1,36 +1,64 @@
 "use strict";
-import nodemailer from 'nodemailer';
+import nodemailer from "nodemailer";
 
 const sendForm = async (req, res) => {
-   if (req.method === "POST") {
-      try {
+  if (req.method === "POST") {
+    try {
       let transporter = nodemailer.createTransport({
-         host: process.env.HOST,
-         port: process.env.PORT,
-         secure: process.env.SECURE, // true for 465, false for other ports
-         auth: {
-           user: process.env.USER,
-           pass: process.env.PASSWORD,
-         },
-       });
+        host: process.env.HOST,
+        port: process.env.PORT,
+        secure: process.env.SECURE, // true for 465, false for other ports
+        auth: {
+          user: process.env.USER,
+          pass: process.env.PASSWORD,
+        },
+      });
 
-       await transporter.sendMail({
-         from: process.env.FROM, // sender address (example: '"Mr. John" <john@someemailaccount.com>')
-         to: process.env.TO, // list of receivers
-         subject: `Message from: ${req.body.fName} ${req.body.lName}`, // Subject line
-         text: `Message from: ${req.body.fName} ${req.body.lName} (${req.body.email}) , sent at: ${new Date().toISOString()} : ${req.body.message}`, // plain text body
-         html: `<p>Message from: <b dir=${(req.body.rtl ? 'rtl' : 'ltr')}>${req.body.fName} ${req.body.lName}</b> (<b>${req.body.email}</b>), sent at <b>${new Date().toISOString()}</b> : </p><p dir=${(req.body.rtl ? 'rtl' : 'ltr')}>${req.body.message}</p><p> - - - - - - -</p><b>Technical message request data: </b><p>${JSON.stringify(req.headers)}</p>` , // html body
-       });
+      await transporter.sendMail({
+        from: process.env.FROM, // sender address (example: '"Mr. John" <john@someemailaccount.com>')
+        to: process.env.TO, // list of receivers
+        subject: `Message from: ${req.body.fName} ${req.body.lName}`, // Subject line
+        text: `Message from: ${req.body.fName} ${req.body.lName} (${
+          req.body.email
+        }) , sent at: ${new Date().toISOString()} : ${req.body.message}`, // plain text body
+        html: `<p>Message from: <b dir=${req.body.rtl ? "rtl" : "ltr"}>${
+          req.body.fName
+        } ${req.body.lName}</b> (<b>${
+          req.body.email
+        }</b>), sent at <b>${new Date().toISOString()}</b> : </p><p dir=${
+          req.body.rtl ? "rtl" : "ltr"
+        }>${
+          req.body.message
+        }</p><p> - - - - - - -</p><b>Technical message request data: </b><p>${JSON.stringify(
+          req.headers
+        )}</p>`, // html body
+      });
 
-       res.status(200).send({"message": "Thank you, your message was sent successfully and I will reply soon."});
-      } catch(error) {
-         console.log(error);
-         res.status(500).send({"message": `Sorry, the message didn't go through for some reason and the error was reported. But, you can still email me to ${process.env.SUPPORT_EMAIL} . :)`});
-      }
-
-   }  else {
-      res.status(404).send("Sorry, what you're looking for isn't here.");
-   }
-}
+      res
+        .status(200)
+        .send({
+          message:
+            "Thank you, your message was sent successfully and I will reply soon.",
+        });
+    } catch (error) {
+      console.log(error);
+      res
+        .status(500)
+        .send({
+          message: `Sorry, the message didn't go through for some reason and the error was reported. But, you can still email me to ${process.env.SUPPORT_EMAIL} . :)`,
+        });
+    }
+  } else {
+    res.status(404).send("Sorry, what you're looking for isn't here.");
+  }
+};
 
 export default sendForm;
+
+export const config = {
+  api: {
+    bodyParser: {
+      sizeLimit: `500kb`
+    }
+  }
+}
