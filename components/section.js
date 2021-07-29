8466301cf5a5c91b322f1327e033d30860477079
unknown <inariwind@gmail.com>
2021-07-29 20:41:56 +0300

    Code refactor.

--- components/section.js
+++ components/section.js
@@ -10,32 +10,62 @@ import Projects from "./projects.js";
 const Section = (props) => {
   const [textPosition, setTextPosition] = useState(false);
   const textScaleY = useRef(null);
-
   props.id && getPosition(props.id, setTextPosition, textPosition); // Gets the section's text top position on screen, in rounded percents.
   useMemo(() => {
     return textScaler(textPosition, textScaleY); // Scales the text according to it's position on screen.
   }, [textPosition]);
   parallax(props.offset, props.setOffset); // Passes the current and new offset parameters to the parallax function.
 
+  // All the CSS values are located in the CSS file unless dynamic CSS values are needed here in the 'style' object.
+
+  const style = {
+    body: {
+      backgroundColor: props.backgroundColor,
+      paddingTop: props.paddingTop,
+      zIndex: props.zIndex,
+      transform: `translateY(${props.offset * props.offsetValue}px)`,
+      transition: props.id === 3 && "0.3s",
+    },
+    imageBackground: {
+      transform: props.id === 1 && `translateY(${props.offset * 0.1}px)`,
+    },
+    sectionShadows: {
+      display: props.id === 1 && "none",
+    },
+    textBackground: {
+      display: props.id === 1 && "block",
+      backgroundImage:
+        props.id === 1
+          ? "linear-gradient(45deg, #ff0099, rgba(0, 0, 0, 0.7), #6303B1)"
+          : "transparent",
+    },
+    title: {
+      display: props.displayText && "block",
+      transition: ".3s ease-out",
+      transform:
+        textScaleY &&
+        `scaleY(${textScaleY.current}) translateY(${props.offset * -0.1}px)`,
+    },
+    subtitle: {
+      display: props.displayText && "block",
+      transition: ".6s ease-out",
+      transform:
+        textScaleY &&
+        `scaleY(${textScaleY.current}) translateY(${props.offset * -0.1}px)`,
+    },
+    smallSubtitle: {
+      display: props.displayText && "block",
+      transition: ".8s ease-out",
+      transform:
+        textScaleY &&
+        `scaleY(${textScaleY.current}) translateY(${props.offset * -0.1}px)`,
+    },
+  };
+
   return (
-    <div
-      className={classes.body}
-      style={{
-        backgroundColor: props.backgroundColor,
-        paddingTop: props.paddingTop,
-        zIndex: props.zIndex,
-        transform: `translateY(${props.offset * props.offsetValue}px)`,
-        transition: props.id === 3 && "0.3s",
-      }}
-    >
+    <div className={classes.body} style={style.body}>
       {props.src && (
-        /* The following <div> is a visual test and can be deleted if not necessary. */
-        <div
-          className={classes.imageBackground}
-          style={{
-            transform: props.id === 1 && `translateY(${props.offset * 0.1}px)`,
-          }}
-        >
+        <div className={classes.imageBackground} style={style.imageBackground}>
           <Image
             src={props.src}
             alt={props.alt}
@@ -45,10 +75,7 @@ const Section = (props) => {
           />
         </div>
       )}
-      <div
-        className={classes.sectionShadows}
-        style={{ display: props.id === 1 && "none" }}
-      />
+      <div className={classes.sectionShadows} style={style.sectionShadows} />
 
       {props.id === 3 && (
         <Table offset={props.offset} offsetValue={props.offsetValue} />
@@ -56,61 +83,18 @@ const Section = (props) => {
 
       <div id={props.id}>
         {props.id === 4 && <Projects />}
-        {/* The following <div> is a visual test and can be deleted if not necessary. Before that it was just <>. */}
-        <div
-          className={classes.textBackground}
-          style={{
-            display: props.id === 1 && "block",
-            backgroundImage:
-              props.id === 1
-                ? "linear-gradient(45deg, #ff0099, rgba(0, 0, 0, 0.7), #6303B1)"
-                : "transparent",
-          }}
-        />
-        <h1
-          className={classes.title}
-          style={{
-            display: props.displayText && "block",
-            transition: ".3s ease-out",
-            transform:
-              textScaleY &&
-              `scaleY(${textScaleY.current}) translateY(${
-                props.offset * -0.1
-              }px)`,
-          }}
-        >
+        <div className={classes.textBackground} style={style.textBackground} />
+        <h1 className={classes.title} style={style.title}>
           {props.title}
         </h1>
 
         {props.subtitle && (
-          <h3
-            className={classes.subtitle}
-            style={{
-              display: props.displayText && "block",
-              transition: ".6s ease-out",
-              transform:
-                textScaleY &&
-                `scaleY(${textScaleY.current}) translateY(${
-                  props.offset * -0.1
-                }px)`,
-            }}
-          >
+          <h3 className={classes.subtitle} style={style.subtitle}>
             {props.subtitle}
           </h3>
         )}
         {props.smallSubtitle && (
-          <h5
-            className={classes.smallSubtitle}
-            style={{
-              display: props.displayText && "block",
-              transition: ".8s ease-out",
-              transform:
-                textScaleY &&
-                `scaleY(${textScaleY.current}) translateY(${
-                  props.offset * -0.1
-                }px)`,
-            }}
-          >
+          <h5 className={classes.smallSubtitle} style={style.smallSubtitle}>
             {props.smallSubtitle}
           </h5>
         )}
