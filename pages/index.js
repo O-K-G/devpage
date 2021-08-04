1098777e3cc7a4004843f91fce5e8ab1f2fbeba1
unknown <inariwind@gmail.com>
2021-08-05 01:14:28 +0300

    Simplify CSS. Add footer.

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import Section from "../components/section";
 import Form from "../components/form";
 import StatusModal from "../components/statusModal";
+import Image from "next/image";
 
 const Home = () => {
   const [offset, setOffset] = useState(0);
   const [open, setOpen] = useState(false);
-  const [sentStatus, setSentStatus] = useState({sent: false, message: "", backgroundImage: "transparent"});
+  const myRef = useRef(null);
+  const [sentStatus, setSentStatus] = useState({
+    sent: false,
+    message: "",
+    backgroundImage: "transparent",
+  });
+
+  const scrollToToP = () => {
+    return myRef.current.scrollIntoView({ behavior: 'smooth' });
+  }
 
   return (
     <>
       <header></header>
-      <main>
+      <main ref={myRef}>
         <Section
           id={1}
           title="MAIN TITLE"
@@ -56,16 +66,34 @@ const Home = () => {
           title="MAIN TITLE"
           subtitle="TEXT TEXT TEXT"
           smallSubtitle="TEXT TEXT TEXT TEXT"
-          paddingTop="50vh"
           zIndex="4"
-          offsetValue="-0.05"
+          offsetValue="-0.08"
           offset={offset}
           setOffset={setOffset}
           backgroundColor="#000000"
         />
       </main>
-      {open && <Form open={open} setOpen={setOpen} sentStatus={sentStatus} setSentStatus={setSentStatus} />}
-      <footer></footer>
+      {open && (
+        <Form
+          open={open}
+          setOpen={setOpen}
+          sentStatus={sentStatus}
+          setSentStatus={setSentStatus}
+        />
+      )}
+      <footer>
+      <div className="footerLogo" onClick={scrollToToP}>
+      <Image
+            src="/images/cat.svg"
+            alt="cat"
+            height="1080"
+            width="1920"
+            quality={100}
+            priority={true}
+          />
+          </div>
+        <p className="footerText">Powered by Purrrrrrrrrrs &#169; {new Date().getFullYear() !== 2021 ? (`2021 - ${new Date().getFullYear()}`) : new Date().getFullYear()}</p>
+      </footer>
       <StatusModal sentStatus={sentStatus} />
     </>
   );
