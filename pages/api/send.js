d0960be43fe9ef6b664d249e1c44a9d5f591659b
unknown <inariwind@gmail.com>
2021-08-07 02:49:57 +0300

    Add external resolver.

--- pages/api/send.js
+++ pages/api/send.js
@@ -14,46 +14,45 @@ const sendForm = async (req, res) => {
       req.body.lName.length > 0 &&
       req.body.email.length > 0
     ) {
-      try {
-        let transporter = nodemailer.createTransport({
-          host: process.env.HOST,
-          port: process.env.PORT,
-          secure: process.env.SECURE,
-          auth: {
-            user: process.env.USER,
-            pass: process.env.PASSWORD,
-          },
-        });
+      let transporter = nodemailer.createTransport({
+        host: process.env.HOST,
+        port: process.env.PORT,
+        secure: process.env.SECURE,
+        auth: {
+          user: process.env.USER,
+          pass: process.env.PASSWORD,
+        },
+      });
 
-        const ip =
-          req.headers["x-forwarded-for"] ||
-          "".split(",")[0] ||
+      const ip =
+        req.headers["x-forwarded-for"] ||
+        "".split(",")[0] ||
         req.connection.remoteAddress;
 
-        http.get(
-          `http://api.ipstack.com/${ip ? ip : undefined}?access_key=${
-            process.env.IPSTACK_API_KEY
-          }`,
-          (response) => {
-            response.on("data", async (data) => {
-              const ipGeoData = { clientPublicIPData: JSON.parse(data) };
-              const { city, region_name, country_name, zip } =
-                ipGeoData.clientPublicIPData;
+      http.get(
+        `http://api.ipstack.com/${ip ? ip : undefined}?access_key=${
+          process.env.IPSTACK_API_KEY
+        }`,
+        (response) => {
+          response.on("data", async (data) => {
+            const ipGeoData = { clientPublicIPData: JSON.parse(data) };
+            const { city, region_name, country_name, zip } =
+              ipGeoData.clientPublicIPData;
 
-                const iPStatusMessage = ip ?
-                `<p style="display: inline">
+            const iPStatusMessage = ip
+              ? `<p style="display: inline">
                     <b style="color: #000000">This message was sent from the IP address (without certainty): </b>
                 </p>
-                <p style="display: inline; color: #ffffff">${ip}</p>` :
-                `<p style="color: #000000">Attempt to retrieve the origin IP address of the message failed.</p>`;
+                <p style="display: inline; color: #ffffff">${ip}</p>`
+              : `<p style="color: #000000">Attempt to retrieve the origin IP address of the message failed.</p>`;
 
-                const location = country_name ?
-                `<p style="display: inline">
+            const location = country_name
+              ? `<p style="display: inline">
                     <b style="color: #000000">Approximated message location of origin (without certainty): </b>
                 </p>
-                <p style="display: inline; color: #ffffff">${city}, ${region_name}, ${country_name} ${zip}</p>` :
-                `<p style="color: #000000">No message approximated location of origin found.</p>`;
-
+                <p style="display: inline; color: #ffffff">${city}, ${region_name}, ${country_name} ${zip}</p>`
+              : `<p style="color: #000000">No message approximated location of origin found.</p>`;
+            try {
               await transporter.sendMail({
                 from: process.env.FROM, // sender address (example: '"Mr. John" <john@someemailaccount.com>')
                 to: process.env.TO, // list of receivers
@@ -74,17 +73,19 @@ const sendForm = async (req, res) => {
                         background-image: linear-gradient(to bottom right, #37cfdc 0%, #5a88e5 100%)"
                   >
                         <div>${iPStatusMessage}</div>
-                        <div style="margin-top: ${country_name ? "4px" : "0"}">${location}</div>
+                        <div style="margin-top: ${
+                          country_name ? "4px" : "0"
+                        }">${location}</div>
                 </div>`, // html body
               });
-            });
-          }
-        );
-        res.status(200).send();
-      } catch (error) {
-        console.log(error);
-        res.status(500).send();
-      }
+              res.status(200).send();
+            } catch (error) {
+              console.log(error);
+              res.status(500).send();
+            }
+          });
+        }
+      );
     } else {
       res
         .status(400) // In case that for some reason more than 50\300 or 0 characters are sent to the backend.
@@ -102,5 +103,6 @@ export const config = {
     bodyParser: {
       sizeLimit: `500kb`,
     },
+    externalResolver: true,
   },
 };
